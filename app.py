818525ca7015b904bc12597710a881306c62f892
aboud20-meet <aboud20@meet.mit.edu>
2018-10-25 18:47:48 +0300

    sue her side

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return None
+    return render_template("home.html")
+
+@app.route('/shop')
+def shop():
+	whales = ["Blue Whale - 10,000,000£", "Sperm Whale - 12,500,000£", "White Whale - 13,400,000£"]
+	return render_template("shop.html", whales = whales)
 
 if __name__ == '__main__':
    app.run(debug = True)
